5713322e4872438ed96fe237ac22a831dae837dd
David <omgemjey@gmail.com>
2019-11-09 21:45:54 +0100

    animation

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
